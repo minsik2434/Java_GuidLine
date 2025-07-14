f375d6fd30aca8024608985a9e4ea3fd1647ded3
minsik2434 <minsik2434@gmail.com>
2025-07-15 01:29:24 +0900

    싱글톤 패턴

---
+++ src/singleton/DataSourceStaticFactory.java
@@ -0,0 +1,16 @@
+package singleton;
+
+public class DataSourceStaticFactory {
+    private static final DataSourceStaticFactory INSTANCE = new DataSourceStaticFactory();
+
+    private DataSourceStaticFactory(){
+    }
+
+    public static DataSourceStaticFactory getInstance(){
+        return INSTANCE;
+    }
+
+    private Object readResolve(){
+        return INSTANCE;
+    }
+}
---
+++ src/singleton/DatabaseSourceFinal.java
@@ -0,0 +1,7 @@
+package singleton;
+
+public class DatabaseSourceFinal {
+    public static final DatabaseSourceFinal INSTANCE = new DatabaseSourceFinal();
+    private DatabaseSourceFinal(){
+    }
+}
