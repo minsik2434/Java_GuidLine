5eee338e186059559c01bcab3699b7e9930bbd42
minsik2434 <minsik2434@gmail.com>
2025-07-15 14:11:27 +0900

    Dependency Injection

---
+++ src/dependency_injection/DatabaseConnectionDI.java
@@ -0,0 +1,17 @@
+package dependency_injection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnectionDI {
+    private final DatabaseSource databaseSource;
+
+    public DatabaseConnectionDI(DatabaseSource databaseSource){
+        this.databaseSource = databaseSource;
+    }
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection("jdbc...", "root", "test");
+    }
+}
---
+++ src/dependency_injection/DatabaseSource.java
@@ -0,0 +1,4 @@
+package dependency_injection;
+
+public interface DatabaseSource {
+}
