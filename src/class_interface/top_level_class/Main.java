dd0caefea301a305940b8bc95fff6e8fd958defb
minsik2434 <minsik2434@gmail.com>
2025-07-19 19:10:43 +0900

    topClass

---
+++ src/class_interface/top_level_class/Main.java
@@ -0,0 +1,10 @@
+package class_interface.top_level_class;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utensil.NAME + Dessert.NAME);
+    }
+}
---
+++ src/class_interface/top_level_class/Utensil.java
@@ -0,0 +1,9 @@
+package class_interface.top_level_class;
+
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
