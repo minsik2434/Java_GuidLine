39c53a8d9ffade02cef38af0660865650f78d48b
minsik2434 <minsik2434@gmail.com>
2025-07-20 13:56:45 +0900

    EnumSet

---
+++ src/enum_annotation/enumset/TextEnumSet.java
@@ -0,0 +1,9 @@
+package enum_annotation.enumset;
+
+import java.util.Set;
+
+public class TextEnumSet {
+    public enum Style {BOLD, ITALIC, UNDERLINE, STRIKETHROUGH}
+    public void applyStyles(Set<Style> styles){
+    }
+}
